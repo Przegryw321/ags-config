afb4ef7351848704e3b9e44cfb6814ef7de35b60
jakub <jakubzp@gmail.com>
2024-09-17 22:45:54 +0200

    active workspace background is now drawn with a gradient

--- ts/widgets/workspaces.ts
+++ ts/widgets/workspaces.ts
@@ -1,61 +1,70 @@
-import Cairo from "gi://cairo";
-import Gtk from "gi://Gtk?version=3.0";
-import Pango from "gi://Pango?version=1.0";
+import Cairo from "types/@girs/cairo-1.0/cairo-1.0";
+import Gtk from "types/@girs/gtk-3.0/gtk-3.0";
+import Pango from "types/@girs/pango-1.0/pango-1.0";
 const PangoCairo = imports.gi.PangoCairo;
 
 const Hyprland = await Service.import('hyprland');
 
-const dummyWs             = Widget.Box({ className: 'workspace' });
-const dummyActiveWs       = Widget.Box({ className: 'workspace-active' });
+// TO COLOR GRADIENT:
+// background-color -> border-color
+
+const dummyWs = Widget.Box({ className: 'workspace' });
+const dummyActiveWs = Widget.Box({ className: 'workspace-active' });
 const dummySecondActiveWs = Widget.Box({ className: 'workspace-active-second' })
-const dummyOccupiedWs     = Widget.Box({ className: 'workspace-occupied' });
+const dummyOccupiedWs = Widget.Box({ className: 'workspace-occupied' });
 
 const draw_workspaces = (area: any, cr: any, count: number) => {
+  const textOffset = +1;
+
   cr.setAntialias(Cairo.Antialias.BEST);
 
   const { width, height } = area.get_allocation();
 
   const wsStyleContext = dummyWs.get_style_context();
-  const wsDiameter     = wsStyleContext.get_property('min-width', Gtk.StateFlags.NORMAL);
-  const wsRadius       = wsDiameter / 2;
-  const wsFontFamily   = wsStyleContext.get_property('font-family', Gtk.StateFlags.NORMAL);
-  const wsFontSize     = wsStyleContext.get_property('font-size', Gtk.StateFlags.NORMAL);
-  const wsfg           = wsStyleContext.get_property('color', Gtk.StateFlags.NORMAL);
+  const wsDiameter = wsStyleContext.get_property('min-width', Gtk.StateFlags.NORMAL);
+  const wsRadius = wsDiameter / 2;
+  const wsFontFamily = wsStyleContext.get_property('font-family', Gtk.StateFlags.NORMAL);
+  const wsFontSize = wsStyleContext.get_property('font-size', Gtk.StateFlags.NORMAL);
+  const wsfg = wsStyleContext.get_property('color', Gtk.StateFlags.NORMAL);
 
   const occupiedStyleContext = dummyOccupiedWs.get_style_context();
-  const occupiedfg           = occupiedStyleContext.get_property('color', Gtk.StateFlags.NORMAL);
-  const occupiedbg           = occupiedStyleContext.get_property('background-color', Gtk.StateFlags.NORMAL);
+  const occupiedfg = occupiedStyleContext.get_property('color', Gtk.StateFlags.NORMAL);
+  const occupiedbg = occupiedStyleContext.get_property('background-color', Gtk.StateFlags.NORMAL);
 
-  const activeWsCenterY = height / 2;
-  const activeRadius    = wsRadius / 1.2;
+  //const activeWsCenterY = height / 2;
+  const activeRadius = wsRadius / 1.2;
   // arbitrarily smaller radius for decorative dot
-  const indicatorRadius = activeRadius / 5;
+  //const indicatorRadius = activeRadius / 5;
 
   const widgetWidth = wsDiameter * count;
   const widgetOffset = (width - widgetWidth) / 2;
 
   const activeMainStyleContext = dummyActiveWs.get_style_context();
-  const activeMainFG           = activeMainStyleContext.get_property('color', Gtk.StateFlags.NORMAL);
-  const activeMainBG           = activeMainStyleContext.get_property('background-color', Gtk.StateFlags.NORMAL);
-  const activeMainWs           = area.attribute.activeWorkspaces[0];
-  const activeMainWsCenterX    = wsDiameter * activeMainWs - wsRadius + widgetOffset;
+  const activeMainFG = activeMainStyleContext.get_property('color', Gtk.StateFlags.NORMAL);
+  const activeMainBG = activeMainStyleContext.get_property('background-color', Gtk.StateFlags.NORMAL);
+  const activeMainBC = activeMainStyleContext.get_property('border-color', Gtk.StateFlags.NORMAL);
+  const activeMainWs = area.attribute.activeWorkspaces[0];
+  //const activeMainWsCenterX    = wsDiameter * activeMainWs - wsRadius + widgetOffset;
 
   const activeSecondStyleContext = dummySecondActiveWs.get_style_context();
-  const activeSecondFG           = activeSecondStyleContext.get_property('color', Gtk.StateFlags.NORMAL);
-  const activeSecondBG           = activeSecondStyleContext.get_property('background-color', Gtk.StateFlags.NORMAL);
-  const activeSecondWs           = area.attribute.activeWorkspaces[1];
-  const activeSecondWsCenterX    = wsDiameter * activeSecondWs - wsRadius + widgetOffset;
+  const activeSecondFG = activeSecondStyleContext.get_property('color', Gtk.StateFlags.NORMAL);
+  const activeSecondBG = activeSecondStyleContext.get_property('background-color', Gtk.StateFlags.NORMAL);
+  const activeSecondBC = activeSecondStyleContext.get_property('border-color', Gtk.StateFlags.NORMAL);
+  const activeSecondWs = area.attribute.activeWorkspaces[1];
+  //const activeSecondWsCenterX    = wsDiameter * activeSecondWs - wsRadius + widgetOffset;
 
   area.set_size_request(wsDiameter * count, wsDiameter);
 
   // Font
-  const layout   = PangoCairo.create_layout(cr);
+  const layout = PangoCairo.create_layout(cr);
   const fontDesc = Pango.font_description_from_string(`${wsFontFamily[0]} ${wsFontSize}`);
   layout.set_font_description(fontDesc);
 
   const wsMask = area.attribute.workspaceMask;
   const wsCenterY = height / 2;
   for (let i = 1; i <= count; ++i) {
+    const wsCenterX = (wsDiameter * i) - wsRadius + widgetOffset;
+
     // Draw occupied workspace background
     if (wsMask & (1 << i)) {
       cr.setSourceRGBA(occupiedbg.red, occupiedbg.green, occupiedbg.blue, occupiedbg.alpha);
@@ -63,7 +72,6 @@ const draw_workspaces = (area: any, cr: any, count: number) => {
       // The extra +1 to the rectangle width is to fix the 1 pixel gap that is
       // created otherwise. The most likely culprit is rounding error.
 
-      const wsCenterX = (wsDiameter * i) - wsRadius + widgetOffset;
       if (!(wsMask & (1 << (i - 1)))) { // Draw (
         cr.arc(wsCenterX + 1, wsCenterY, wsRadius, 0.5 * Math.PI, -0.5 * Math.PI);
         cr.fill();
@@ -84,33 +92,50 @@ const draw_workspaces = (area: any, cr: any, count: number) => {
       cr.setSourceRGBA(wsfg.red, wsfg.green, wsfg.blue, wsfg.alpha);
     }
 
+    // draw active background
+    if (i === activeMainWs) {
+      const pattern = new Cairo.LinearGradient(
+        wsCenterX - wsRadius,
+        wsCenterY - wsRadius,
+        wsCenterX + wsRadius,
+        wsCenterY + wsRadius,
+      );
+      pattern.addColorStopRGBA(0.2, activeMainBG.red, activeMainBG.green, activeMainBG.blue, activeMainBG.alpha);
+      pattern.addColorStopRGBA(1.0, activeMainBC.red, activeMainBC.green, activeMainBC.blue, activeMainBC.alpha);
+
+      cr.setSource(pattern);
+
+      cr.arc(wsCenterX, wsCenterY, activeRadius, 0, 2 * Math.PI);
+      cr.fill();
+
+      cr.setSourceRGBA(activeMainFG.red, activeMainFG.green, activeMainFG.blue, activeMainFG.alpha);
+    } else if (i === activeSecondWs) {
+      const pattern = new Cairo.LinearGradient(
+        wsCenterX - wsRadius,
+        wsCenterY - wsRadius,
+        wsCenterX + wsRadius,
+        wsCenterY + wsRadius,
+      );
+      pattern.addColorStopRGBA(0.2, activeSecondBG.red, activeSecondBG.green, activeSecondBG.blue, activeSecondBG.alpha);
+      pattern.addColorStopRGBA(1.0, activeSecondBC.red, activeSecondBC.green, activeSecondBC.blue, activeSecondBC.alpha);
+
+      cr.setSource(pattern);
+
+      cr.arc(wsCenterX, wsCenterY, activeRadius, 0, 2 * Math.PI);
+      cr.fill();
+
+      cr.setSourceRGBA(activeSecondFG.red, activeSecondFG.green, activeSecondFG.blue, activeSecondFG.alpha);
+    }
+
     // Draw workspace number
     layout.set_text(`${i}`, -1);
     const [layoutWidth, layoutHeight] = layout.get_pixel_size();
     const x = (wsDiameter * i) - (layoutWidth / 2) - wsRadius + widgetOffset;
-    const y = (height - layoutHeight) / 2;
+    const y = (height - layoutHeight) / 2 + textOffset;
     cr.moveTo(x, y);
     PangoCairo.show_layout(cr, layout);
     cr.stroke();
   }
-
-  // Draw main active workspace
-  cr.setSourceRGBA(activeMainBG.red, activeMainBG.green, activeMainBG.blue, activeMainBG.alpha);
-  cr.arc(activeMainWsCenterX, activeWsCenterY, activeRadius, 0, 2 * Math.PI);
-  cr.fill();
-
-  cr.setSourceRGBA(activeMainFG.red, activeMainFG.green, activeMainFG.blue, activeMainFG.alpha);
-  cr.arc(activeMainWsCenterX, activeWsCenterY, indicatorRadius, 0, 2 * Math.PI);
-  cr.fill();
-
-  // Draw second active workspace
-  cr.setSourceRGBA(activeSecondBG.red, activeSecondBG.green, activeSecondBG.blue, activeSecondBG.alpha);
-  cr.arc(activeSecondWsCenterX, activeWsCenterY, activeRadius, 0, 2 * Math.PI);
-  cr.fill()
-
-  cr.setSourceRGBA(activeSecondFG.red, activeSecondFG.green, activeSecondFG.blue, activeSecondFG.alpha);
-  cr.arc(activeSecondWsCenterX, activeWsCenterY, indicatorRadius, 0, 2 * Math.PI);
-  cr.fill();
 };
 
 const dispatch = (id: number) => Hyprland.messageAsync(`dispatch workspace ${id}`);
@@ -144,10 +169,10 @@ const Workspaces = (count: number = 10) => Widget.Box({
 
   setup: self => self
     .hook(Hyprland.active.workspace, self => {
-      const mainMonitor   = Hyprland.monitors[0]?.activeWorkspace.id;
+      const mainMonitor = Hyprland.monitors[0]?.activeWorkspace.id;
       const secondMonitor = Hyprland.monitors[1]?.activeWorkspace.id;
 
-      if (mainMonitor)   self.attribute.activeWorkspaces[0] = mainMonitor;
+      if (mainMonitor) self.attribute.activeWorkspaces[0] = mainMonitor;
       if (secondMonitor) self.attribute.activeWorkspaces[1] = secondMonitor;
 
       if (mainMonitor || secondMonitor) self.queue_draw();
