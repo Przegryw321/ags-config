b2ba65d86a13916a62145746e664aaf3184d091f
jakub <jakubzp@gmail.com>
2024-09-23 15:30:03 +0200

    added colors to bartime

--- ts/widgets/bartime.ts
+++ ts/widgets/bartime.ts
@@ -4,8 +4,8 @@ const Hyprland = await Service.import('hyprland');
 
 export const DateTimeString = (format: string) => Glib.DateTime.new_now_local().format(format);
 
-export const DateTimeLabel = (format: string, { ...rest } = {}) => Widget.Label({
-  ...rest,
+export const DateTimeLabel = ({ format, ...props }) => Widget.Label({
+  ...props,
   label: DateTimeString(format),
 
   setup: self => self.poll(1000, self => {
@@ -13,15 +13,86 @@ export const DateTimeLabel = (format: string, { ...rest } = {}) => Widget.Label(
   }),
 });
 
-export const Clock = () => DateTimeLabel('%H:%M', {
-  className: 'bartime-clock',
-  hpack: 'end',
-  vpack: 'center',
+export const DateTimeStyleLabel = ({ format, style, mainClassName, ...props }) => Widget.Label({
+  ...props,
+  label: DateTimeString(format),
+
+  setup: self => self.poll(1000, self => {
+    self.label = DateTimeString(format) || '';
+    self.class_name = `${mainClassName} ${style(self.label)}`;
+  }),
 });
-export const Date = () => DateTimeLabel('%A, %d %B', {
-  className: 'bartime-date',
+
+export const Clock = () => DateTimeLabel({
+  className: 'bartime-clock',
+  format: '%H:%M',
   hpack: 'end',
-  vpack: 'center',
+});
+
+export const Date = () => DateTimeLabel({
+  className: 'bartime-date',
+  format: '%A, %d %B',
+  hpack: 'end',
+});
+
+export const Weekday = () => DateTimeStyleLabel({
+  mainClassName: 'bartime-weekday',
+  format: '%A',
+
+  style: (label: string) => {
+    const weekday = label.slice(0, 3);
+    switch (weekday) {
+      case 'pon': return 'monday';
+      case 'wto': return 'tuesday';
+      case 'śro': return 'wednesday';
+      case 'czw': return 'thursday';
+      case 'pią': return 'friday';
+      case 'sob': return 'saturday';
+      case 'nie': return 'sunday';
+    }
+  },
+});
+
+export const MonthDay = () => DateTimeLabel({
+  className: 'bartime-day',
+  format: ', %d ',
+});
+
+export const Month = () => DateTimeStyleLabel({
+  mainClassName: 'bartime-month',
+  format: '%B',
+
+  style: (label: string) => {
+    const month = label.slice(0, 3);
+    switch (month) {
+      case 'sty':
+      case 'lut':
+      case 'gru':
+        return 'winter';
+      case 'mar':
+      case 'kwi':
+      case 'maj':
+        return 'spring';
+      case 'cze':
+      case 'lip':
+      case 'sie':
+        return 'summer';
+      case 'wrz':
+      case 'paź':
+      case 'lis':
+        return 'autumn';
+    }
+  }
+});
+
+export const StyleDate = () => Widget.Box({
+  className: 'bartime-date',
+
+  children: [
+    Weekday(),
+    MonthDay(),
+    Month(),
+  ]
 });
 
 export const BarTime = ({ ... rest } = {}) => Widget.Box({
@@ -30,7 +101,7 @@ export const BarTime = ({ ... rest } = {}) => Widget.Box({
   vertical: true,
   children: [
     Clock(),
-    Date(),
+    StyleDate(),
   ],
 });
 
