75c2a88b3e4666e4eb9d237de8a5b0057870e7bf
jakub <jakubzp@gmail.com>
2024-12-04 21:00:59 +0100

    removed leftover print statement

--- variables/player.ts
+++ variables/player.ts
@@ -10,7 +10,6 @@ export const ActivePlayer = Variable(null as AstalMpris.Player | null)
     .observe(Mpris, 'player-closed', () => {
         const players = Mpris.get_players()
 
-        print('players:', players)
         if (players.length > 0)
             return players[players.length - 1]
 
