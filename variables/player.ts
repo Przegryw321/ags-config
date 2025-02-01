d4ed70c8fd47352e7057919019bcd32a0bc1f290
jakub <jakubzp@gmail.com>
2025-02-01 18:29:10 +0100

    changed how active player is set

--- variables/player.ts
+++ variables/player.ts
@@ -1,10 +1,10 @@
-import { Variable } from "astal"
+import { Variable, bind } from "astal"
 import AstalMpris from "gi://AstalMpris"
 import Mpris from "../utils/player"
 
 export const ActivePlayer = Variable(null as AstalMpris.Player | null)
     .observe(Mpris, 'player-added', (_mpris, player) => {
-        ActivePlayer.observe(player, 'notify', player => player)
+        bind(player, 'playback-status').subscribe(() => ActivePlayer.set(player))
         return player
     })
     .observe(Mpris, 'player-closed', () => {
@@ -19,7 +19,7 @@ export const ActivePlayer = Variable(null as AstalMpris.Player | null)
 const players = Mpris.get_players()
 
 for (const player of players)
-    ActivePlayer.observe(player, 'notify', player => player)
+    bind(player, 'playback-status').subscribe(() => ActivePlayer.set(player))
 
 if (players.length > 0)
     ActivePlayer.set(players[players.length - 1])
