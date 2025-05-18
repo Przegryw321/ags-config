9ece2f29cdd4905592b029d6fb9326ea991a1b3d
jakub <jakubzp@gmail.com>
2025-05-18 17:10:06 +0200

    fixed null error on DeepBindLabel

--- widgets/misc.tsx
+++ widgets/misc.tsx
@@ -9,6 +9,6 @@ export function DeepBindLabel({ binding, property, ...props }: DeepBindProps) {
     // wrapped in a box so it can be used as a JSX element
     return <box>{binding.as(obj => {
         if (!obj) return <label visible={false}/>
-        return <label truncate label={bind(obj, property)} {...props}/>
+        return <label truncate label={bind(obj, property).as(v => v ?? '')} {...props}/>
     })}</box>
 }
