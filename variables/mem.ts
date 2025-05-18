495a168c93b50c2db015855a4cd9f769633e112d
jakub <jakubzp@gmail.com>
2025-05-18 23:06:06 +0200

    changed how MemPercentage is calculated

--- variables/mem.ts
+++ variables/mem.ts
@@ -1,14 +1,9 @@
 import { readFileAsync, Variable } from "astal";
 
 export const MemPercentage = Variable(0)
-  .poll(5000, async () => {
-    return await readFileAsync('/proc/meminfo').then(contents => {
-      const lines = contents.split('\n')
-      const totalLine = lines[0].split(' ')
-      const total = Number(totalLine[totalLine.length - 2])
-      const availableLine = lines[2].split(' ')
-      const available = Number(availableLine[availableLine.length - 2])
-      const used = total - available
-      return Math.round(used / total * 1000) / 10
-    })
+  .poll(5000, ["sh", "-c", "free | head -n-1 | tail -n1 | xargs"], (out, _prev) => {
+    const values = out.split(' ')
+    const total = Number(values[1])
+    const used = Number(values[2])
+    return Math.round(used / total * 1000) / 10
   })
