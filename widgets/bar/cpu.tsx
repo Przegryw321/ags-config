9ebacbec8ceb9eda59745dbbdb52ae87325234b8
jakub <jakubzp@gmail.com>
2025-05-19 12:46:49 +0200

    small fixes

--- widgets/bar/cpu.tsx
+++ widgets/bar/cpu.tsx
@@ -15,7 +15,7 @@ export default function BarCpu({ ...props }: BarCpuProps) {
         const c1 = get_property(style, 'border-top-color') as unknown as RGBA
         const c2 = get_property(style, 'border-left-color') as unknown as RGBA
         const c3 = get_property(style, 'border-bottom-color') as unknown as RGBA
-        const color = gradient3(c1, c2, c3, load / CpuCount.get())
+        const color = gradient3(c1, c2, c3, load / (CpuCount.get() || 12))
         self.css = `color: rgba(${color.red * 255}, ${color.green * 255}, ${color.blue * 255}, ${color.alpha})`
       })
     }} />
